73b3469dd044266b170dad1a06ad76817cd3564f
hongjuns <asws12345@naver.com>
2020-09-13 22:01:26 +0900

    Webpack Ex_1

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+    * 1.원의 넓의를 구하는 공식
+    * 2. PI 정의
+    * 3. 공식을 통해 결과 얻기
+*/ 
+
+const mathUtil = require('./mathUtil');
+
+const result = mathUtil.getCircleArea(2);
+console.log(result);
+console.log(mathUtil.PI);
