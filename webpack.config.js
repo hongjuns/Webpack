fa4bb0589181ac39e59774153b7eeaffa5431657
hongjuns <asws12345@naver.com>
2020-09-21 22:55:44 +0900

    commit_20200921

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry :'./src/index.js',
+    entry :'./src/webindex.js',
     output : {
         path : path.resolve(__dirname,'dist'),
         filename:'bundle.js'
     },
-    target:'node'
+    mode:'none'
 }
